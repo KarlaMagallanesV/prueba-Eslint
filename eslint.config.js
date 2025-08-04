33f4ca27bf1c1825da47f422c403c480157192a4
karlamagallanesv <karla.magallanes.vicente@vallegrande.edu.pe>
2025-08-03 20:36:19 -0500

    feat: agregar configuración de ESLint para reglas personalizadas y soporte de TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,18 @@ const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
+// Importa tus reglas personalizadas
+const customRules = require("./eslint-rules/custom-rules.js");
+
 module.exports = tseslint.config(
+  // Configuración para archivos TypeScript
   {
     files: ["**/*.ts"],
+    plugins: {
+      custom: {
+        rules: customRules.rules, // Carga tus reglas aquí
+      },
+    },
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -14,6 +23,7 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
+      // Reglas de Angular
       "@angular-eslint/directive-selector": [
         "error",
         {
@@ -30,8 +40,15 @@ module.exports = tseslint.config(
           style: "kebab-case",
         },
       ],
+
+      // Tus reglas personalizadas
+      "custom/no-console-log": "error",
+      "custom/prefer-const-strict": "warn",
+      "custom/require-todo-author": "warn",
     },
   },
+
+  // Configuración para archivos HTML (plantillas)
   {
     files: ["**/*.html"],
     extends: [
